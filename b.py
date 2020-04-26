a561bd0218d1517d6fa9b069647a2bd18411b1ac
gaur <shashankgaur48@gmail.com>
2020-04-26 13:28:06 +0530

    remove old files

--- b.py
+++
@@ -1,9 +0,0 @@
-
-import praw
-import pprint
-reddit = praw.Reddit()
-
-#assume you have a Reddit instance bound to variable `reddit`
-submission = reddit.submission(url='https://www.reddit.com/r/india/comments/fvz69t/mom_came_up_with_this_impressive_idea_to/')
-print(submission.title) # to make it non-lazy
-pprint.pprint(vars(submission))
